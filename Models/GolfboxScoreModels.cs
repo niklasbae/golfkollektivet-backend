54332b2b5525b7d82cd35661d84a48ecbaf372a0
Niklas Bae Pedersen <niklas.pedersen@banenor.no>
2025-03-31 16:44:48 +0200

    fix for 9 holes

--- Models/GolfboxScoreModels.cs
+++ Models/GolfboxScoreModels.cs
@@ -20,7 +20,7 @@ public class SubmitScoreRequest
     [Required, RegularExpression(@"\d{2}:\d{2}", ErrorMessage = "Format: HH:mm")]
     public string ScoreTime { get; set; } = default!;
 
-    [Required, MinLength(18), MaxLength(18)]
+    [Required, MinLength(9), MaxLength(18)]
     public List<int> HoleScores { get; set; } = new();
     
 }
